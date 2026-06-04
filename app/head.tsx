3b2f3a4b21ddc3f45fda900c6ccca1c7a458de14
lksarmiento67 <tu-email@asociado-a-lksarmiento67.com>
2026-06-04 15:11:53 -0300

    first commit

--- app/head.tsx
+++ app/head.tsx
@@ -1,3 +1,17 @@
 export default function Head() {
-  return <meta name="test-head" content="test-meta" />;
+  if (!process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID) return null;
+
+  return (
+    <>
+      <meta
+        name="google-adsense-account"
+        content={process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID}
+      />
+      <script
+        async
+        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID}`}
+        crossOrigin="anonymous"
+      ></script>
+    </>
+  );
 }
