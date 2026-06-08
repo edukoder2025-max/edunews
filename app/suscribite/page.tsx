3cc89b7c047702ad993f2467cdb839ade0f5870a
lksarmiento67 <tu-email@asociado-a-lksarmiento67.com>
2026-06-08 02:23:25 -0300

    console

--- app/suscribite/page.tsx
+++ app/suscribite/page.tsx
@@ -34,7 +34,6 @@ export default function SubscribePage() {
           basicSubscriptions.init({
             type: "NewsArticle",
             isPartOfType: ["Product"],
-            isPartOfProductId: "${publicationId}:openaccess",
             clientOptions: { theme: "light", lang: "es-419" },
           });
 
